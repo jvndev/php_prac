f53afd1ba6f11a1843c1eb8022ee3198351a3082
Jaco <jvndev@gmail.com>
2024-10-26 16:34:32 +0200

    Prac

--- di.php
+++ di.php
@@ -1,11 +1,8 @@
 <?php
 
-namespace DI;
-
-use PDO;
-
-class Person {
-    public function __construct(public string $first_name, public string $last_name) {}
+class Person
+{
+    protected function __construct(public string $first_name, public string $last_name) {}
 
     #[\Override]
     public function __toString()
@@ -14,70 +11,79 @@ class Person {
     }
 }
 
-interface IDataSource {
+interface IDataSource
+{
     public function getData(): array;
 }
 
-class DB implements IDataSource {
+class DB implements IDataSource
+{
     private readonly PDO $pdo;
 
     public function __construct()
     {
-       $this->pdo = new PDO(
-        'mysql:host=localhost;dbname=jaco;',
-        'jaco',
-        'password',
-        [
-            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ,
-        ]
-       ); 
+        $this->pdo = new PDO(
+            'mysql:host=localhost;dbname=jaco;',
+            'jaco',
+            'password',
+            [
+                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+            ]
+        );
     }
 
     #[\Override]
-    public function getData(): array {
-        $arr = [];
-        $stmt = $this->pdo->query("select first_name, last_name from employees");
-
-        foreach ($stmt->fetchAll() as $record)
-            array_push($arr, new Person($record->first_name, $record->last_name));
-
-        return $arr;
+    public function getData(): array
+    {
+        return $this->pdo
+            ->query("select first_name, last_name from employees")
+            ->fetchAll();
     }
 }
 
-class Text implements IDataSource {
-    private const FILE = __DIR__."/people";
+class Text implements IDataSource
+{
+    private const FILE = __DIR__ . "/people";
 
     #[\Override]
-    public function getData(): array {
+    public function getData(): array
+    {
         $lines = explode("\n", file_get_contents(Text::FILE));
 
         return array_map(function ($line) {
             preg_match_all("/[^;]+/", $line, $matches);
             $matches = $matches[0];
 
-            return new Person($matches[0], $matches[1]);
+            return [
+                "first_name" => $matches[0],
+                "last_name" => $matches[1],
+            ];
         }, $lines);
     }
 }
 
-class Data {
+class Data extends Person
+{
     private readonly array $people;
 
     public function __construct(IDataSource $dataSource)
     {
-       $this->people = $dataSource->getData(); 
+        $this->people = array_map(function ($assoc) {
+            return new Person($assoc['first_name'], $assoc['last_name']);
+        }, $dataSource->getData());
     }
 
-    public function getPeople(): array {
+    public function getPeople(): array
+    {
         return $this->people;
     }
 }
 
-function arrToStr(array $arr): string {
+function arrToStr(array $arr): string
+{
     if (!count($arr)) return "empty";
 
-    return array_reduce($arr, function($p, $c) {
+    return array_reduce($arr, function ($p, $c) {
         return ($p ? "$p, " : "") . $c;
     }, "");
 }
