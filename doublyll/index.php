d09316148def776667084f12c3be6139c2e0b68b
Jaco <jvndev@gmail.com>
2024-10-31 14:31:51 +0200

    Prac

--- doublyll/index.php
+++ doublyll/index.php
@@ -11,6 +11,45 @@ if (!isset($_SESSION['DLL'])) {
 }
 
 define('DLL', $_SESSION['DLL']);
+define('CONN', new DB());
+
+class DB
+{
+    private const CONNPROPS = [
+        'host' => 'localhost',
+        'user' => 'jaco',
+        'pass' => 'password',
+        'db'   => 'jaco',
+    ];
+    private readonly mysqli $conn;
+
+    public function __construct()
+    {
+        $this->conn = mysqli_connect(...array_values(self::CONNPROPS));
+    }
+
+    private function _keyResults(array $res): array
+    {
+        return array_reduce($res, function (array $p, array $c) {
+            $p[$c['id']] = array_filter(
+                $c,
+                fn($v, string $k) => $k != 'id',
+                ARRAY_FILTER_USE_BOTH
+            );
+
+            return $p;
+        }, []);
+    }
+
+    public function getNumbers()
+    {
+        return $this->_keyResults(
+            $this->conn
+                ->query('select id, phpsessid, nr from numbers')
+                ->fetch_all(MYSQLI_ASSOC)
+        );
+    }
+}
 
 function html()
 {
@@ -38,6 +77,14 @@ function html()
     <body>
         <input id='txtNr'>
         <button id='btnNr'>Send</button>
+        <form method="get" action=".">
+            <input name='txtGetTest'>
+            <button name="btnGetTest">GET</button>
+        </form>
+        <form method="post" action=".">
+            <input name='nr'>
+            <button name="btnPostTest">POST</button>
+        </form>
     </body>
 
     </html>
@@ -45,12 +92,17 @@ function html()
 <?php
 }
 
-function api() {
+function api()
+{
     echo $_POST['nr'];
 }
 
 if (count($_POST)) {
     api();
+} elseif (count($_GET)) {
+    var_dump(CONN->getNumbers());
 } else {
     html();
 }
+
+?>
--- test2.php
+++ test2.php
@@ -1,24 +1,6 @@
 <?php
 
-class C {
-    public const CONST = 42;
+$arr = [1 => [1,2,3]];
+$arr[] = [2 => [4,5,6]];
 
-    public function __construct(private string $a,) { }
-
-    public function getA() { return $this->a;}
-
-    #[Override]
-    public function __toString()
-    {
-        return $this->a;
-    }
-}
-
-$c = 'CONST';
-
-echo ((new C('42'))->getA());
-echo (new C('a'))::{$c};
-echo new C('zzz');
-
-echo ord('a');
-echo chr(97);
+var_dump($arr);
